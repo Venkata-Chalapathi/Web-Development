3804e53b9c51704481722de9b821ad6df2797220
G Venkata Chalapathi <thegvc971@gmail.com>
2025-08-10 20:24:44 +0530

    Add files via upload

---
+++ Backend/script.js
@@ -0,0 +1,42 @@
+
+// let n = 5;
+
+// for(let i = 0; i < n; i++){
+//     console.log(i);
+// }
+
+// console.log("Hello");
+
+// let ars = process.argv;
+
+// for(let i = 2; i < ars.length; i++){
+//     console.log(ars[i]);
+// }
+
+// const someval = require("./math");
+// console.log(someval.mul(5,7));
+
+// const getfruits = require("./fruits");
+
+// console.log(getfruits[0].name);
+
+
+
+// const figlet = require("figlet");
+
+// figlet ("Wenktt_" , function (err, data) {
+//     if(err){
+//         console.log(err);
+
+//     }
+//     console.log(data);
+// })
+
+
+import {sum,mul,pi} from "./math.js";
+import { generate } from "random-words";
+
+console.log(generate());
+
+// console.log(sum(1,2));
+
